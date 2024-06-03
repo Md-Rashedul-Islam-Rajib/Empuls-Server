f08a941414615f0459a61ea911e333dfe1c9e68a
rajib5570 <rajib5570@gmail.com>
2024-06-03 22:03:59 +0600

    get api for service data added

--- index.js
+++ index.js
@@ -30,14 +30,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+
+    const servicesCollection = client.db('serviceDB').collection('service')
+
+// * get api for services
+
+app.get('/services',async(req,res)=> {
+  const cursor = servicesCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
