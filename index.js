e643e5d0211b0dd838df16534d8f90b9103acbb6
rajib5570 <rajib5570@gmail.com>
2024-06-09 02:48:33 +0600

    jwt related api added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 
 
 app.use(express.json());
@@ -40,6 +41,32 @@ async function run() {
     const testimonialsCollection = client.db('serviceDB').collection('testimonials');
     const usersCollection = client.db('serviceDB').collection('users');
     const workCollection = client.db('serviceDB').collection('work');
+
+
+            // jwt related api
+            app.post('/jwt', async(req,res)=>{
+              const user = req.body;
+              const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: "6h"});
+              res.send({token});
+            });
+
+            // middlewares
+            const verifyToken = (req,res,next) => {
+              console.log("inside",req.headers);
+              if(!req.headers.authorization){
+                return res.status(401).send({message: 'Forbidden Access'})
+              }
+              const token = req.headers.authorization.split(' ')[1];
+              jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+                if(err){
+                  return res.status(401).send({message: 'forbidden access'})
+                }
+                req.decoded = decoded;
+                next();
+              })
+            }
+
+
 
 
 // * get api for services
@@ -88,6 +115,7 @@ app.post('/work-list', async (req,res)=> {
 
 // * get api for user info
 app.get('/users',async(req,res)=> {
+  
   const email = req.query.email
   if(email){
 
@@ -118,6 +146,23 @@ app.put('/users/:id',async (req,res)=>{
 })
 
 
+// ? put api for update employee fired status
+app.patch('/users/:id',async (req,res)=>{
+  const id = req.params.id;
+ 
+  console.log(id);
+  const filter = {_id : new ObjectId(id)};
+  const options = {upsert : true};
+  const updatedData = {
+    $set: {
+ isFired: true
+ }
+  };
+  const result = await usersCollection.updateOne(filter,updatedData,options);
+  res.status(200).send(result);
+})
+
+
 // ? patch api for update verify status
 app.patch('/users/:id',async (req,res)=>{
   const id = req.params.id;
@@ -136,6 +181,7 @@ app.patch('/users/:id',async (req,res)=>{
 
 // * get api for work data
 app.get('/work-list', async(req,res)=> {
+  
   const email = req.query.email;
   const query = { email : email};
   const cursor =  workCollection.find(query).sort({ date: -1 });
